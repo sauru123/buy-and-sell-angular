3f1e2990ebd2b4250221c4b32099e5de868c689b
sauru123 <saurabhkshatriya2@gmail.com>
2023-02-02 22:11:48 +0530

    env var set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAbqJlxbrymPqgSMevDTZDrkjd5S6FTlEo",
+    authDomain: "buy-and-sell-1d417.firebaseapp.com",
+    projectId: "buy-and-sell-1d417",
+    storageBucket: "buy-and-sell-1d417.appspot.com",
+    messagingSenderId: "276411424995",
+    appId: "1:276411424995:web:ae6c98390d1152d61965d6",
+    measurementId: "G-BBS6839Z83"
+  }
 };
